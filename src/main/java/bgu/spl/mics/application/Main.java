607df1f0972a3472948de63d2e2476021b5b5f20
yonathan95 <yonathan.wolloch@gmail.com>
2020-11-30 21:40:38 +0200

    Finished beginning tests

--- src/main/java/bgu/spl/mics/application/Main.java
+++ src/main/java/bgu/spl/mics/application/Main.java
@@ -2,7 +2,10 @@ package bgu.spl.mics.application;
 
 import bgu.spl.mics.Future;
 import bgu.spl.mics.application.messages.AttackEvent;
+import com.google.gson.Gson;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.HashMap;
 
 /** This is the Main class of the application. You should parse the input file,
@@ -11,6 +14,15 @@ import java.util.HashMap;
  */
 public class Main {
 	public static void main(String[] args) {
+		Gson gson = new Gson();
+		HashMap input;
+		try {
+			input = gson.fromJson(new FileReader(System.getProperty("user.dir")+"//input.json"),HashMap.class);
+			System.out.println(input);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
 
 	}
 }
